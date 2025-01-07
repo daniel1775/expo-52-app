19f694ce6fe888cad2dea6722023649c683d82d3
fpena <fpena@Daniel-Nieto-Air.local>
2025-01-07 11:04:29 -0500

    chore: nativewind initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-	extends: ['expo', 'prettier'],
+	extends: ['expo', 'prettier', 'universe/native'],
 	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': 'error',
